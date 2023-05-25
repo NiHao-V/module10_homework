f0cacedebd6c21bd922b79d659c27c67dd68863f
NiHao-V <134102103+NiHao-V@users.noreply.github.com>
2023-05-25 12:20:05 +0300

    Task 6 added

---
+++ 6.js
@@ -0,0 +1,12 @@
+const array = [4, 5, 4];
+let a = true;
+for (let i = 1; i < array.length; i++) {
+   if (array[0] !== array[i]) {
+    a = false;
+    } 
+}
+   if (a === false) {
+     console.log(false);
+   } else {
+     console.log(true);
+   }
