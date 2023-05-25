49b8a2b20e89c0d4133207b852491a1bb5b680af
NiHao-V <134102103+NiHao-V@users.noreply.github.com>
2023-05-25 12:22:58 +0300

    Task 6 update

--- 6.js
+++ 6.js
@@ -2,11 +2,11 @@ const array = [4, 5, 4];
 let a = true;
 for (let i = 1; i < array.length; i++) {
    if (array[0] !== array[i]) {
-    a = false;
+      a = false;
     } 
 }
-   if (a === false) {
-     console.log(false);
-   } else {
-     console.log(true);
-   }
+if (a === false) {
+    console.log(false);
+} else {
+    console.log(true);
+}
